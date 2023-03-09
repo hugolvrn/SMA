fa3f01a551720a7d875bef32f1f9691ba381660d
hugolvrn <125551089+hugolvrn@users.noreply.github.com>
2023-03-09 11:07:26 +0100

    Update environment.py

--- src/environment.py
+++ src/environment.py
@@ -1,3 +1,5 @@
 import numpy as np
 
-stations = np.array(['PMP','PR','K','RER','U','D'])
+stations = np.array(['PONTSMEUNIERPERRO','PLACEROSE','KLAY','RER','SUPERU','DESCARTESSUD'])
+s = np.array(['PMP','PR','K','RER','U','D'])
+travel_times = np.array([])
